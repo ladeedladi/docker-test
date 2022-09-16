1d76194d239d1127dd1d158db200bf914f55d0f4
Ladeed <muhammed.ladeed123@gmail.com>
2022-09-16 21:39:05 +0530

    Added depends_on and envs

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const { MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PASSWORD } = require("./config/config")
 const app = express()
 
-mongoose.connect("mongodb://lad:sikk@mongo/?authSource=admin").then(() => { console.log("connected to mongo") }).catch(err => { console.log(err) })
+let mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}/?authSource=admin`
+
+const connectwithRetry = () => {
+    mongoose.connect(mongoUrl)
+        .then(() => console.log("connected to mongo"))
+        .catch(err => {
+            console.log("err", err)
+            setTimeout(connectwithRetry, 5000)
+        })
+}
+connectwithRetry()
 app.get("/", (req, res) => {
-    res.send("<h1>Sikk sdddddss <h1>")
+    res.send("<h1>Sikk  <h1>")
 })
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log("Runing ....." + port)
-})
+}) 
